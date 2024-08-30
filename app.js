5c40fbde862e0a627fdff8b6a7b49c6294ebfce3
Roe Nøttseter <roe@nottseter.no>
2024-08-30 12:57:54 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const sqlite3 = require('better-sqlite3')
+const db = sqlite3('./sql/studietid.db', {verbose: console.log})
+
+let result = addUser('Roe', 'Nøttseter', 3, 0, 'roe@nottseter.no')
+
+function addUser(firstName, lastName, idRole, isAdmin, email)
+ {
+
+    let sql = db.prepare("INSERT INTO user (firstName, lastName, idRole, isAdmin, email) " + 
+                         " values (?, ?, ?, ?, ?)")
+    const info = sql.run(firstName, lastName, idRole, isAdmin, email)
+    
+    sql = db.prepare('SELECT user.id as userid, firstname, lastname, role.name  as role FROM user inner join role on user.idrole = role.id   WHERE user.id  = ?');
+    let rows = sql.all(info.lastInsertRowid)
+    console.log("rows.length",rows.length)
+
+    return rows[0]
+}
