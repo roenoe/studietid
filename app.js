b9e02a02d1aa82b90de02689deea93a2fb678996
Roe Nøttseter <roe@nottseter.no>
2024-09-03 13:31:55 +0200

    added a bunch of functions

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const sqlite3 = require('better-sqlite3')
 const db = sqlite3('./sql/studietid.db', {verbose: console.log})
 
-let result = addUser('roenoe', '', 1, 1, 'roe@admin.com')
+let result = addUser('Per', 'Hansen', 3, 0, 'per@hansen.no')
 
 function addUser(firstName, lastName, idRole, isAdmin, email)
  {
+    let check = checkEmail(email)
+    if (check) {
+        return check
+    }
 
     let sql = db.prepare("INSERT INTO user (firstName, lastName, idRole, isAdmin, email) " + 
                          " values (?, ?, ?, ?, ?)")
@@ -16,3 +20,58 @@ function addUser(firstName, lastName, idRole, isAdmin, email)
 
     return rows[0]
 }
+
+function checkEmail(email) {
+    var at = 0
+    var dot = 0
+    let sql = db.prepare('SELECT * FROM user WHERE email = ?')
+    let rows = sql.all(email)
+    for (var i = 0; i < email.length; i++) {
+        if (email[i] == "@") {
+            at += 1
+        }
+        if (at > 0 && email[i] == ".") {
+            dot += 1
+        }
+        if (email[i] == " ") {
+            return {error: 'Invalid email'}
+        }
+    }
+    if (at != 1 || dot < 1) {
+        return {error: 'Invalid email'}
+    }
+    if (rows.length > 0) {
+        return {error: 'Email already exists'}
+    }
+    return false
+
+}
+
+function getIdByEmail(email) { 
+    let sql = db.prepare('SELECT id FROM user WHERE email = ?')
+    let rows = sql.all(email)
+    return rows[0].id
+}
+
+function delUser(id) {
+    let sql = db.prepare('DELETE FROM user WHERE id = ?')
+    sql.run(id)
+}
+
+let activity = registerActivity(6, '2024-09-03 16:00:00', 1, 1, 1, 90)
+
+function registerActivity(idUser, startTime, idSubject, idRoom, idStatus, duration) {
+    let check = checkActivity(idUser, startTime)
+    let sql = db.prepare("INSERT INTO activity (idUser, startTime, idSubject, idRoom, idStatus, duration) " + 
+                         " values (?, ?, ?, ?, ?, ?)")
+    sql.run(idUser, startTime, idSubject, idRoom, idStatus, duration)
+}
+
+/*function checkActivity(idUser, startTime) {
+    let sql = db.prepare('SELECT * FROM activity WHERE idUser = ? AND startTime = ?')
+    let rows = sql.all(idUser, startTime)
+    if (rows.length > 0) {
+        return {error: 'Activity already exists'}
+    }
+    return false
+}*/
